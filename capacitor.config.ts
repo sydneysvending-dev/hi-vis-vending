77278f04d59b60c8ff8b9a81704a42877c17e712
Byron Jones <byronjones05@icloud.com>
2025-09-10 10:25:10 +1000

    Initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'au.com.sydneyselectvending.app',
+  appId: 'com.hivis.vending',
   appName: 'Hi-Vis Vending',
-  webDir: 'dist/public'
+  webDir: 'dist',
+  server: {
+    url: 'http://192.168.68.50:3000',
+    cleartext: true
+  }
 };
 
 export default config;
